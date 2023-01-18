c6c00abf777884ff6f79a7bac431e4edb88a16a3
Aaron Moon <112985603+aemoon03@users.noreply.github.com>
2023-01-18 15:42:39 -0800

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -8,7 +8,7 @@ class Handler implements URLHandler {
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
+            return String.format("Aaron's number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
@@ -21,7 +21,7 @@ class Handler implements URLHandler {
                     return String.format("Number increased by %s! It's now %d", parameters[1], num);
                 }
             }
-            return "404 Not Found!";
+            return "404 Not Found!";    
         }
     }
 }
